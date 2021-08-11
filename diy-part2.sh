bbf68efb72f1685dc464f6a76221d3f53646b0e4
mss-x <44271544+mss-x@users.noreply.github.com>
2021-08-11 10:46:23 +0800

    Update diy-part2.sh
    修改默认后台地址192.168.1.1为10.0.0.2

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP（修改默认后台地址192.168.1.1为10.0.0.2）
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
