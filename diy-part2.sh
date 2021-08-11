8996930f8cbe3a888d3933f1f7dfe200f0f43438
mss-x <44271544+mss-x@users.noreply.github.com>
2021-08-11 12:31:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP（修改默认后台地址192.168.1.1为10.0.0.2）
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+#修改机器名称
+sed -i 's/OpenWrt/MSS-X/g' package/base-files/files/bin/config_generate
 #添加额外软件包
 ##git clone https://github.com/P3TERX/xxx package/xxx
 #git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
