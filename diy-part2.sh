64c5b3bae900e8ca9aa32e36bc7536c5a96de8e3
mss-x <44271544+mss-x@users.noreply.github.com>
2021-08-12 13:05:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,32 +10,32 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP（修改默认后台地址192.168.1.1为10.0.0.2）
+# Modify default IP（修改默认后台地址192.168.1.1为10.0.0.2）+
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
-#修改机器名称
+#修改机器名称+
 sed -i 's/OpenWrt/MSS-X/g' package/base-files/files/bin/config_generate
 #####################################################################
 #移除不用软件包
 #解锁网易云灰色歌曲3合1新版本
-rm -rf package/lean/luci-app-unblockmusic
+#rm -rf package/lean/luci-app-unblockmusic
 #广告屏蔽大师Plus +
-rm -rf package/lean/luci-app-adbyby-plus
+#rm -rf package/lean/luci-app-adbyby-plus
 #访问时间控制
-rm -rf package/lean/luci-app-accesscontrol
+#rm -rf package/lean/luci-app-accesscontrol
 #UU网游加速器
-rm -rf package/lean/luci-app-uugamebooster
+#rm -rf package/lean/luci-app-uugamebooster
 #迅雷快鸟
-rm -rf package/lean/luci-app-xlnetacc
+#rm -rf package/lean/luci-app-xlnetacc
 #网络共享?
-rm -rf package/lean/luci-app-samba
+#rm -rf package/lean/luci-app-samba
 #FTP 服务器
-rm -rf package/lean/luci-app-vsftpd
+#rm -rf package/lean/luci-app-vsftpd
 #qBittorrent
-rm -rf package/lean/luci-app-qbittorrent
+#rm -rf package/lean/luci-app-qbittorrent
 #IPSec VPN 服务器
-rm -rf package/lean/luci-app-ipsec-vpnd
+#rm -rf package/lean/luci-app-ipsec-vpnd
 #网络唤醒
-rm -rf package/lean/luci-app-wol
+#rm -rf package/lean/luci-app-wol
 #####################################################################
 #添加额外软件包
 #git clone https://github.com/P3TERX/xxx package/xxx
